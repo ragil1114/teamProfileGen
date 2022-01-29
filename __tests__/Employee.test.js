06200cf4c70a67d38d6c16d6c070dcbe224300b7
Ragil14 <rahil.gilani@gmail.com>
2022-01-29 14:32:36 -0600

    added tests to Emplyee.test.js file

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require('./lib/Employee');
+
+test('creates an employee object', () => {
+    const name = 'John';
+    const id = 1;
+    const email = 'JohnDoe@gmail.com'
+    const employee = new Employee(name, id, email);
+    expect(typeof employee).toBe('object');
+});
+
+test('correctly gets the employee name', () => {
+    const name = 'John';
+    const id = 1;
+    const email = 'JohnDoe@gmail.com'
+    const employee = new Employee(name, id, email);
+    expect(employee.getName()).toBe(name);
+});
+
+test('correctly gets the employee id', () => {
+    const name = 'John';
+    const id = 1;
+    const email = 'JohnDoe@gmail.com'
+    const employee = new Employee(name, id, email);
+    expect(employee.getId()).toBe(id);
+});
+
+test('correctly gets the employee role', () => {
+    const name = 'John';
+    const id = 1;
+    const email = 'JohnDoe@gmail.com'
+    const employee = new Employee(name, id, email);
+    expect(employee.getRole()).toBe('Employee');
+});
+
+
+
